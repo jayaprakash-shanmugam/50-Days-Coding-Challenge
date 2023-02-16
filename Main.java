d51b59eba7d213d631fe4025db197518e79ea592
jayaprakash shanmugam <96587553+jayaprakash-shanmugam@users.noreply.github.com>
2023-02-16 20:17:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+Code chef : Pet Store - same multiset of animals for both or not?
+
+Solution [Java]
+---------------
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int[] type = new int[n];
+            for(int i = 0; i<n; i++){
+                type[i] = sc.nextInt();
+            }
+            Arrays.sort(type);
+            if(n%2 ==0 ){
+                int count = 0;
+                for(int i = 0; i<n; i+=2){
+                    if(type[i] == type[i+1]){
+                        count++;
+                    }
+                }
+                if(count == n/2){
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO");
+                }
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+	}
+}
